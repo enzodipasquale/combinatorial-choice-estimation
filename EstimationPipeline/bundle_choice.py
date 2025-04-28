44c8f11ff561c94ba26ec4171e795d56c4029700
ed2189 <ed2189@nyu.edu>
2025-04-28 14:44:42 -0400

    cont

--- EstimationPipeline/bundle_choice.py
+++ EstimationPipeline/bundle_choice.py
@@ -1,8 +1,9 @@
 from mpi4py import MPI
 import numpy as np
+import gurobipy as gp
 
 class BundleChoice:
-    def __init__(self, data, config):
+    def __init__(self, data, dims, config, compute_features):
 
         self.comm = MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
@@ -10,16 +11,27 @@ class BundleChoice:
 
         self.data = data
         self.config = config
+        self.num_agents, self.num_items, self.num_features = dims
 
 
-        # Unpack config
-        self.tol_certificate = config["tol_certificate"]
-        self.max_slack_counter = config["max_slack_counter"]
-        self.tol_row_generation = config["tol_row_generation"]
-        self.row_generation_decay = config["row_generation_decay"]
-        self.num_simulations = config["num_simulations"]
-        self.max_iters = config["max_iters"]
-        self.min_iters = config["min_iters"]
+        # Unpack config 
+        self.tol_certificate = float(config["tol_certificate"])
+        self.max_slack_counter = int(config["max_slack_counter"])
+        self.tol_row_generation = int(config["tol_row_generation"])
+        self.row_generation_decay = float(config["row_generation_decay"])
+        self.num_simuls = int(config["num_simuls"])
+        self.max_iters = int(config["max_iters"])
+        if config["min_iters"] is None:
+            self.min_iters = np.log(self.tol_certificate / (self.tol_row_generation - 1)) / np.log(self.row_generation_decay)
+        else:
+            self.min_iters = config["min_iters"]
+
+        self._compute_features = compute_features
+
+
+
+    def compute_features(self, bundles):
+        return self._compute_features(self, bundles)
 
 
 
@@ -29,28 +41,25 @@ class BundleChoice:
         if self.rank == 0:
             self.item_data = self.data.get("item_data", None)
             self.agent_data = self.data.get("agent_data", None)
-            self.errors = self.data.get("errors", None)
+            self.error_s_i_j = self.data.get("errors", None)
+            self.error_si_j = self.error_s_i_j.reshape(self.num_simuls * self.num_agents, self.num_items) if self.error_s_i_j is not None else None
 
-            dims = self.errors.shape if self.errors is not None else None
+            self.obs_bundle = self.data["obs_bundle"]
         else:
             self.item_data = None
-            dims = None
 
         # scatter item_data to all ranks
         self.item_data = self.comm.bcast(self.item_data, root=0)
-        self.num_simul, self.num_agents, self.num_objs = self.comm.bcast(dims, root=0)
 
         # scatter agent_data and errors in chunks
         if self.rank == 0:
-            i_chunks = np.array_split(np.tile(np.arange(self.num_agents), self.num_simul), self.comm_size)
-            si_chunks = np.array_split(np.arange(self.num_simul * self.num_agents), self.comm_size)
+            i_chunks = np.array_split(np.tile(np.arange(self.num_agents), self.num_simuls), self.comm_size)
+            si_chunks = np.array_split(np.arange(self.num_simuls * self.num_agents), self.comm_size)
 
-            errors_flat = self.errors.reshape(self.num_simul * self.num_agents, self.num_objs) if self.errors is not None else None
-
-            data_chunks = [{
+            data_chunks = [{"agent_indeces": i_chunks[r],
                             "agent_data": {key : value[i_chunks[r]] for key, value in self.agent_data.items()} 
                                             if self.agent_data is not None else None,
-                            "errors": errors_flat[si_chunks[r],:,None]
+                            "errors": self.error_si_j[si_chunks[r],:,None] if self.error_si_j is not None else None
                             }
                             for r in range(self.comm_size)
                             ]
@@ -58,11 +67,41 @@ class BundleChoice:
             data_chunks = None
         
         local_data = self.comm.scatter(data_chunks, root=0)
+        self.local_indeces = local_data["agent_indeces"]
         self.local_errors = local_data["errors"]
         self.local_agent_data = local_data["agent_data"]
 
 
+    def initialize_master(self):
 
+        master_pb = gp.Model('GMM_pb')
+        master_pb.setParam('Method', 0)
+        # master_pb.setParam('LPWarmStart', 2)
+
+        
+
+        # Variables and Objective
+        phi_hat_k = self.compute_features(self.obs_bundle).sum(0)
+
+        master_pb.setAttr('ModelSense', gp.GRB.MAXIMIZE)
+        lambda_k = master_pb.addMVar(self.num_features, obj = phi_hat_k, ub = 1e9 , name='parameter')
+        u_si = master_pb.addMVar(self.num_simuls * self.num_agents, obj = - (1/ self.num_simuls), name='utility')
+        p_j = master_pb.addMVar(self.num_items, obj = -1 , name='price')
+
+        # Initial Constraint (to make problem bounded)
+        phi_i_k_all = self.compute_features(np.ones_like(self.obs_bundle))
+        master_pb.addConstrs((
+                u_si[si] + p_j.sum() >= self.error_si_j[si].sum() + phi_i_k_all[si % self.num_agents, :] @ lambda_k
+                for si in range(self.num_simuls * self.num_agents)
+                            ))
+
+        # Solve master problem
+        master_pb.optimize()
+
+
+
+
+    
     
 
 
--- EstimationPipeline/test.py
+++ EstimationPipeline/test.py
@@ -1,65 +1,113 @@
 from bundle_choice import BundleChoice
 import numpy as np
-
+import yaml
 from mpi4py import MPI
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
+# Load configuration from YAML file
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
-# Define config dictionary (normally loaded from YAML)
-config = {
-    "tol_certificate": 1e-3,
-    "max_slack_counter": 3,
-    "tol_row_generation": 5,
-    "row_generation_decay": 0.5,
-    "num_simulations": 2,
-    "max_iters": 100,
-}
-
-
-
+# Load data on rank 0
 if rank == 0:
-    # # Agent independent data be in any form
-    # item_data = np.array([3,2,3])
+    item_data = {
+                "quadratic" : np.load("./data/quadratic_characteristic_j_j_k.npy"),
+                "weights" : np.load("./data/weight_j.npy")
+                }
 
-    # # Agent specific data is a dictionary with characteristics as keys
+    # Agent specific data is a dictionary with characteristics as keys
+    agent_data = {
+                "modular": np.load("./data/modular_characteristics_i_j_k.npy"),
+                "capacity": np.load("./data/capacity_i.npy"),
+                }
 
-    # agent_data = {
-    #     "modular": np.array([[1, 2, 4], 
-    #                         [5, 6, 8]]),
-    #     "capacity": np.array([10, 20]),
-    # }
-
-    # # errors are a 3D tensor (modular!)
-    # errors = np.array([[[0.1, 0.2, 0.3], 
-    #                     [0.4, 0.5, 0.6]], 
-
-    #                     [[0.7, 0.8, 0.9], 
-    #                     [1.0, 1.1, 1.2]]])
-
-    
+    # errors are a 3D tensor (modular!)
+    np.random.seed(0)
 
     data = {
-        "item_data": item_data,
-        "agent_data": agent_data,
-        "errors": errors
-    }
+            "item_data": item_data,
+            "agent_data": agent_data,
+            "errors": np.random.normal(0,1,size = (config["num_simuls"], 255, 493)),
+            "obs_bundle": np.load("./data/matching_i_j.npy")
+            }
 else:
     data = None
 
-# Compute min_iters
-config["min_iters"] = np.log(config["tol_certificate"] / (config["tol_row_generation"] - 1)) / np.log(config["row_generation_decay"])
+dims = (255, 493, 4)
 
 
 
-####################################################################################################
 
 
-my_test = BundleChoice(data, config=config)
+# Characteristics oracle
+def compute_features(self, bundle_i_j):
+    modular_i_j_k = self.agent_data["modular"]
+    quadratic_j_j_k = self.item_data["quadratic"]
+
+    features_hat_i_k = np.concatenate((
+                                    np.einsum('ijk,ij->ik', modular_i_j_k, bundle_i_j),
+                                    np.einsum('jlk,ij,il->ik', quadratic_j_j_k, bundle_i_j, bundle_i_j)
+                                    ), axis = 1)
+
+    return features_hat_i_k
 
 
+#####################################################################################################
 
+my_test = BundleChoice(data, dims, config, compute_features)
 
 my_test.scatter_data()
 
-print("Rank:", my_test.rank, my_test.item_data)
+# print("Rank:", my_test.rank, my_test.item_data["quadratic"].shape)
+# print("Rank:", my_test.rank, my_test.local_agent_data["modular"].shape)
+
+if rank == 0:
+    my_test.initialize_master()
+    # my_test.compute_features(my_test.obs_bundle)
+
+
+
+# # Pricing problem
+# def init_pricing(self, weight_j, capacity):
+
+#     # Create subproblem
+#     subproblem = gp.Model() 
+#     subproblem.setParam('OutputFlag', 0)
+#     subproblem.setAttr('ModelSense', gp.GRB.MAXIMIZE)
+#     # Create variables
+#     B_j = subproblem.addMVar(len(weight_j), vtype = gp.GRB.BINARY)
+
+#     # Knapsack constraint
+#     subproblem.addConstr(weight_j @ B_j <= capacity)
+#     subproblem.update()
+
+#     return subproblem 
+
+
+
+# def solve_pricing(subproblem, modular_j_k, quadratic_j_j_k ,lambda_k, p_j):
+
+#     ### Define objective from data and master solution 
+#     num_MOD = modular_j_k.shape[1] - 1
+#     L_j =  modular_j_k[:,0] + modular_j_k[:,1:] @ lambda_k[:num_MOD] -  p_j
+#     Q_j_j = quadratic_j_j_k @ lambda_k[num_MOD: ]
+
+#     # Set objective
+#     B_j = subproblem.getVars()
+#     subproblem.setObjective(B_j @ L_j + B_j @ Q_j_j @ B_j)
+
+#     # Solve the updated subproblem
+#     subproblem.optimize()
+#     optimal_bundle = np.array(subproblem.x, dtype=bool)
+#     value = subproblem.objVal
+#     check_gap(subproblem.MIPGap)
+
+#     ### Compute value of characteristics at optimal bundle (1+1+K+J)
+#     row =   np.concatenate(([value],
+#                             (modular_j_k[optimal_bundle]).sum(0), 
+#                             quadratic_j_j_k[optimal_bundle][:, optimal_bundle].sum((0, 1)),
+#                             subproblem.x
+#                             ))
+#     return row
