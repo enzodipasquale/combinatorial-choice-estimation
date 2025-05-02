ad47f537c5c3aa1998fdd72aa106375a671ae150
ed2189 <ed2189@nyu.edu>
2025-05-02 13:52:41 -0400

    dual config working

--- applications/combinatorial_auction/run_estimation.py
+++ applications/combinatorial_auction/run_estimation.py
@@ -18,10 +18,6 @@ INPUT_DIR = os.path.join(BASE_DIR, "input_data")
 IS_LOCAL = os.path.exists("/Users/enzo-macmini") or os.path.exists("/Users/enzo-macbookpro") 
 CONFIG_PATH = os.path.join(BASE_DIR, "config_local.yaml" if IS_LOCAL else "config.yaml")
 
-# print config path
-if rank == 0:
-    print(f"Using config file: {CONFIG_PATH}")
-
 with open(CONFIG_PATH, 'r') as file:
     config = yaml.safe_load(file)
 
