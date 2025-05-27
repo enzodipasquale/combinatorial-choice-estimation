74a48aa8228cd761b70c4f14f643473c74f0822f
Enzo Di Pasquale <ed2189@nyu.edu>
2025-05-27 12:24:53 +0200

    knapsack working

--- bundlechoice/utils.py
+++ bundlechoice/utils.py
@@ -25,28 +25,30 @@ def suppress_output():
 
 def log_iteration(iteration, lambda_k, rank=0):
     if rank == 0:
-        print("#" * 80)
-        print("#" * 80)
+        print("#" * 100)
         print(f"ITERATION: {iteration}")
         print("Time:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 def log_solution(master_pb, lambda_k_iter, rank):
     if rank == 0:
+        print("-"*80)
         print("Solution found:", lambda_k_iter)
+        print("-"*80)
+
         os.makedirs("output", exist_ok=True)
         master_pb.write('output/master_pb.mps')
         master_pb.write('output/master_pb.bas')
 
 
 def log_init_master(self, x_hat_k):
-    print("#"*80)
+    print("#"*100)
     print("PROBLEM DETAILS")
     print("number of agents     :", self.num_agents)
     print("number of items      :", self.num_items)
     print("number of features   :", self.num_features)
     print("number of simulations:", self.num_simuls)
     print("first moments:", x_hat_k)
-    print('-'*80)
+    print('-'*100)
     print("SETTINGS")
     print("tol certificate     :", self.config.tol_certificate)
     print("max slack_counter   : ", self.config.max_slack_counter)
@@ -56,5 +58,5 @@ def log_init_master(self, x_hat_k):
     print("maximum iterations  :", self.config.max_iters)
     print("SLURM")
     print("comm size           :", self.comm_size)
-    print("#"*80)
+    print("#"*100)
 
