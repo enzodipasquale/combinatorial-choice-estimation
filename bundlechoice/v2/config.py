7f3785c3b6f8c64a8263e64e5c3fc679c8b1ce12
Enzo Di Pasquale <ed2189@nyu.edu>
2025-07-21 12:31:37 +0200

    v2 working

--- bundlechoice/v2/config.py
+++ bundlechoice/v2/config.py
@@ -35,14 +35,30 @@ class RowGenConfig:
     """
     item_fixed_effects: bool = False
     tol_certificate: float = 0.01
-    max_slack_counter: Optional[int] = None  
+    max_slack_counter: int = None  # Will be set to float('inf') if None
     tol_row_generation: float = 0.0
     row_generation_decay: float = 0.0
-    max_iters: Optional[int] = None
-    min_iters: int = 0
+    max_iters: int = None  # Will be set to float('inf') if None
+    min_iters: int = 0  
     master_settings: dict = field(default_factory=dict)
-    master_ubs: Optional[dict] = None
-    master_lbs: Optional[list] = None
+    master_ubs: dict = None
+    master_lbs: list = None
+
+    def __post_init__(self):
+        if self.max_slack_counter is None:
+            self.max_slack_counter = float('inf')
+        if self.max_iters is None:
+            self.max_iters = float('inf')
+        if self.min_iters is None:
+            self.min_iters = 0
+        if self.tol_certificate is None:
+            self.tol_certificate = 1e-6
+        if self.tol_row_generation is None:
+            self.tol_row_generation = 0.0
+        if self.row_generation_decay is None:
+            self.row_generation_decay = 0.0
+        if self.master_settings is None:
+            self.master_settings = {}
 
 def load_config(bundle_choice, cfg: dict):
     """
