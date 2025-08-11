d6e8e3b3f57d7df87cd3751deb3a209bdab669b7
Enzo Di Pasquale <ed2189@nyu.edu>
2025-08-11 13:05:50 +0200

    Add QuadKnapsack subproblem to v2 registry

--- applications/combinatorial_auction/run_estimation.py
+++ applications/combinatorial_auction/run_estimation.py
@@ -1,7 +1,6 @@
 #!/bin/env python
 
 from bundlechoice import BundleChoice
-from bundlechoice.subproblems import get_subproblem
 
 import numpy as np
 import yaml
@@ -26,31 +25,43 @@ if rank == 0:
     INPUT_DIR = os.path.join(BASE_DIR, "input_data")
     obs_bundle = np.load(os.path.join(INPUT_DIR, "matching_i_j.npy"))
 
+    num_agents = config["dimensions"]["num_agents"]
+    num_items = config["dimensions"]["num_items"]
+    num_features = config["dimensions"]["num_features"]
+    num_simuls = config["dimensions"]["num_simuls"]
+
     item_data = {
+        "modular": -np.eye(num_items),
         "quadratic": np.load(os.path.join(INPUT_DIR, "quadratic_characteristic_j_j_k.npy")),
         "weights": np.load(os.path.join(INPUT_DIR, "weight_j.npy"))
     }
-
+    # print(item_data["weights"])
     agent_data = {
         "modular": np.load(os.path.join(INPUT_DIR, "modular_characteristics_i_j_k.npy")),
         "capacity": np.load(os.path.join(INPUT_DIR, "capacity_i.npy")),
     }
 
     np.random.seed(1995)
-    errors = np.random.normal(0, 1, size=(config["num_simuls"], config["num_agents"], config["num_items"]))
+    errors = np.random.normal(0, 1, size=(num_simuls, num_agents, num_items))
 
-    data = {
+    input_data = {
         "item_data": item_data,
         "agent_data": agent_data,
         "errors": errors,
         "obs_bundle": obs_bundle
     }
 else:
-    data = None
+    input_data = None
 
-# Run the estimation
-combinatorial_auction = BundleChoice(config)
-combinatorial_auction.load_data(data)
-combinatorial_auction.build_feature_oracle_from_data()
-combinatorial_auction.scatter_data()
-combinatorial_auction.compute_estimator_row_gen()
+# Broadcast dimensions to all ranks
+num_features = comm.bcast(num_features if rank == 0 else None, root=0)
+
+# # Run the estimation
+combinatorial_auction = BundleChoice()
+combinatorial_auction.load_config(CONFIG_PATH)
+combinatorial_auction.data.load_and_scatter(input_data)
+combinatorial_auction.features.build_from_data()
+combinatorial_auction.subproblems.load()
+# combinatorial_auction.subproblems.initialize_local()
+# combinatorial_auction.subproblems.init_and_solve(np.ones(num_features))
+combinatorial_auction.row_generation.solve()
--- bundlechoice/subproblems/subproblem_registry.py
+++ bundlechoice/subproblems/subproblem_registry.py
@@ -1,11 +1,13 @@
 from .registry.linear_knapsack import LinearKnapsackSubproblem
 from .registry.greedy import GreedySubproblem
+from .registry.quadratic_knapsack import QuadraticKnapsackSubproblem
 from .registry.quad_supermodular import QuadraticSOptNetwork, QuadraticSOptLovasz
 from bundlechoice.subproblems.registry.plain_single_item import PlainSingleItemSubproblem
 
 SUBPROBLEM_REGISTRY = {
     "LinearKnapsack": LinearKnapsackSubproblem,
     "Greedy": GreedySubproblem,
+    "QuadKnapsack": QuadraticKnapsackSubproblem,
     "QuadSupermodularNetwork": QuadraticSOptNetwork,
     "QuadSupermodularLovasz": QuadraticSOptLovasz,
     'PlainSingleItem': PlainSingleItemSubproblem,
