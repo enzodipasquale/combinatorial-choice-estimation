de50e1f8ccf26ccdfd7e02fc31518223885f04da
ed2189 <ed2189@nyu.edu>
2026-01-21 12:57:52 -0500

    sync

--- bundlechoice/config.py
+++ bundlechoice/config.py
@@ -38,9 +38,8 @@ class SubproblemConfig(ConfigMixin):
 
 @dataclass
 class RowGenerationConfig(ConfigMixin):
-    tolerance_optimality: float = 1e-06
     max_slack_counter: float = float('inf')
-    tol_row_generation: float = 0.0
+    tol_row_generation: float = 1e-6
     row_generation_decay: float = 1.0
     max_iters: float = float('inf')
     min_iters: int = 0
