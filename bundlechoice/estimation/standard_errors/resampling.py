57f7eb7beeb74e5bd6fc767a16546ba3671125e2
Enzo Di Pasquale <ed2189@nyu.edu>
2026-01-22 17:26:54 +0100

    sync

--- bundlechoice/estimation/standard_errors/resampling.py
+++ bundlechoice/estimation/standard_errors/resampling.py
@@ -74,7 +74,8 @@ class ResamplingMixin:
             'converged': self.result.converged,
             'n_constraints': self.result.n_constraints,
             'pricing_time': total_pricing_time,
-            'master_time': total_master_time
+            'master_time': total_master_time,
+            'reduced_cost': self.result.final_reduced_cost
         })
 
     def _log_bootstrap_iteration(self, bootstrap_iter, theta):
@@ -92,10 +93,10 @@ class ResamplingMixin:
         if bootstrap_iter % 80 == 0:
             param_width = len(param_indices) * 11 - 1
             header1 = (f"{'Boot':>5} | {'Time':^9} | {'Pricing':^9} | {'Master':^9} | {'RG':^5} | "
-                      f"{'#Constr':>7} | {'Objective':^12} | {f'Parameters':^{param_width}}")
+                      f"{'#Constr':>7} | {'Reduced':^12} | {'Objective':^12} | {f'Parameters':^{param_width}}")
             param_label_row = ' '.join(f'{f"θ[{i}]":>10}' for i in param_indices)
             header2 = (f"{'':>5} | {'(s)':^9} | {'(s)':^9} | {'(s)':^9} | {'Iters':^5} | "
-                      f"{'':>7} | {'Value':^12} | {param_label_row}")
+                      f"{'':>7} | {'Cost':^12} | {'Value':^12} | {param_label_row}")
             logger.info(header1)
             logger.info(header2)
             logger.info("-" * len(header1))
@@ -104,9 +105,11 @@ class ResamplingMixin:
         time_str = f"{info['time']:>9.3f}"
         pricing_time_str = f"{info.get('pricing_time', 0.0):>9.3f}"
         master_time_str = f"{info.get('master_time', 0.0):>9.3f}"
+        reduced_cost_str = format_number(info.get('reduced_cost', 0.0), width=12, precision=6)
         row = (f"{bootstrap_iter:>5} | {time_str} | {pricing_time_str} | {master_time_str} | "
                f"{info['iterations']:>5} | "
                f"{info.get('n_constraints', 0):>7} | "
+               f"{reduced_cost_str} | "
                f"{format_number(info['objective'], width=12, precision=5)} | {param_vals}")
         logger.info(row)
 
